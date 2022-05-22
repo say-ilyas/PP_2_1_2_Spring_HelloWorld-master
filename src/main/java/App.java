3aa7735b26c70fcb66da521eb223811b1cd3880b
say-ilyas <102316165+say-ilyas@users.noreply.github.com>
2022-05-22 20:32:29 +0300

    Initial commit

---
+++ src/main/java/App.java
@@ -0,0 +1,26 @@
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class App {
+    public static void main(String[] args) {
+        ApplicationContext applicationContext =
+                new AnnotationConfigApplicationContext(AppConfig.class);
+        HelloWorld bean =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean.getMessage());
+
+        HelloWorld bean1 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean.getMessage());
+
+        Cat beanCat = (Cat) applicationContext.getBean("privet cat");
+        System.out.println(beanCat.getMessage());
+
+        Cat beanCat1 = (Cat) applicationContext.getBean("privet cat");
+        System.out.println(beanCat.getMessage());
+        System.out.println(bean.equals(bean1));
+        System.out.println(beanCat.equals(beanCat1));
+
+    }
+
+}
